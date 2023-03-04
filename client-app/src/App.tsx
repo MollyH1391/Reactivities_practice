e66dd0772cf6bf6093958afffb9e4dbc3f6b0e37
molly huang <molly.huang@wingwill.com.tw>
2023-03-04 15:56:29 +0800

    edit UI

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+import { Header } from 'semantic-ui-react';
 
 function App() {
   const [activities, setActivities] = useState([]);
@@ -16,7 +17,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <Header as='h2' icon='users' content='Reactivities' />
         <img src={logo} className="App-logo" alt="logo" />
         <ul>
           {activities.map((activity: any) => (
@@ -27,7 +28,7 @@ function App() {
 
           }
         </ul>
-      </header>
+      
     </div>
   );
 }
