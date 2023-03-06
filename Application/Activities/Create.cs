a67556d64aa8f278d281d2f0f9668c0e527f76bd
molly huang <molly.huang@wingwill.com.tw>
2023-03-06 23:11:02 +0800

    add details

---
+++ Application/Activities/Create.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Application.Activities
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            
+        }
+    }
+}
